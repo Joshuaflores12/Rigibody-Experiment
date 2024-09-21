facc4a609a9faba30e0b5e76c9abf0a4f4df3bba
Joshuaflores12 <joshuagabriellflores@gmail.com>
2024-09-21 18:31:57 +0800

    Mechanic done

--- Assets/Scripts/BouncingBall.cs
+++ Assets/Scripts/BouncingBall.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class BouncingBall : MonoBehaviour
 {
     public Rigidbody rb;
-    [SerializeField] private float gravityScale = 1.0f;
-    [SerializeField]private static float globalGravity = 9.8f;
-    [SerializeField]private float forceAmount;
+    [SerializeField] private float gravityScale = 1.0f; // rb's Gravity Scale
+    [SerializeField] private static float globalGravity = 9.8f; // Earth's gravity
+    [SerializeField] private float forceAmount; // force applied
+    [SerializeField] private float decayF;      // percentage of loss energy to force
+    [SerializeField] private float minForceAmount; // min force for velocity to come to a hault
+    [SerializeField] private float coefficientOfResititution;
    
 
    
@@ -15,19 +18,31 @@ public class BouncingBall : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        decayF = coefficientOfResititution;
     }
 
     void FixedUpdate()
     {
-        Vector3 gravity = globalGravity * gravityScale * Vector3.down;
-        
+        Vector3 gravity = globalGravity * gravityScale * Vector3.down; // The objects fall speed is determined by gravity
+        rb.AddForce(gravity, ForceMode.Acceleration); // continous gravity application
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            rb.AddForce(Vector3.up * forceAmount, ForceMode.Impulse);
+            decayF = coefficientOfResititution; // COR is based on the elasticity of the material where ranges from 0 - 1 the higher it is the higher the energy is stored and lost upon impact with another object
+            forceAmount *= decayF; // to get the percentage of the energy lost of which will be the new force amount 
+            if (forceAmount > minForceAmount)
+            {
+                rb.AddForce(Vector3.up * forceAmount, ForceMode.Impulse);
+
+            }
+            else 
+            {
+            rb.velocity = Vector3.zero;
+            }
         }
     }
 }
